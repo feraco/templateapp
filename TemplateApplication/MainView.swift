586b4247ef3a6448002585698e6a952b3f85bcbb
Fred Feraco <frederick.feraco@gmail.com>
2023-06-17 14:21:51 -0400

    Initial commit

---
+++ TemplateApplication/MainView.swift
@@ -0,0 +1,32 @@
+//
+// This source file is part of the StanfordBDHG Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import SwiftUI
+
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "hand.wave.fill")
+                .font(.system(size: 100))
+                .foregroundColor(.accentColor)
+                .padding()
+            Text("Welcome to the Template Application!")
+                .bold()
+        }
+    }
+}
+
+
+#if DEBUG
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+#endif
